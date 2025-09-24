b9ad99bf5bff88e927f293c1be3101a05d0db5e0
Mohsin Irshad <isometric.mohsin@gmail.com>
2025-09-24 10:19:01 +0500

    hook updated.

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -2,14 +2,16 @@ import { useEffect } from "react";
 
 export const useBodyScrollLock = (isOpen: boolean) => {
   useEffect(() => {
+    const originalOverflow = document.body.style.overflow;
+
     if (isOpen) {
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "";
+      document.body.style.overflow = originalOverflow;
     }
 
     return () => {
-      document.body.style.overflow = "";
+      document.body.style.overflow = originalOverflow;
     };
   }, [isOpen]);
 };
